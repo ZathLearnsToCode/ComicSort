f67a70cd61fe755a18878152851bcdc86ed9d047
Pascal Mc Gregor <pmcgregor@videotron.ca>
2021-05-09 19:54:09 -0400

    Add Menus Module
    Added a module for menus

---
+++ ComicSort/ComicSort/Modules/ComicSort.Modules.MenusModule/MenusModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace ComicSort.Modules.MenusModule
+{
+    public class MenusModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
