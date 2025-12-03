c953a485f2dbef1fe92c3a9d508481f67a999b3c
Zath <51383195+Zatharus@users.noreply.github.com>
2025-12-02 21:14:02 -0500

    Initial commit

---
+++ ComicSort.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ComicSort.UI.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to ComicSort!";
+    }
+}
---
+++ ComicSort.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComicSort.UI.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
