30a897985a03eccd89da09ca73fda10f04ecaf9b
Zath <51383195+Zatharus@users.noreply.github.com>
2025-12-07 15:49:10 -0500

    Added DTO and Database Entities
    Added DTO and Database Entities for the Comic Books

---
+++ ComicSort.Core/DTO/ComicBookDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicSort.Core.DTO
+{
+    public class ComicBookDTO
+    {
+        public Guid Id { get; set; }
+        public string FilePath { get; set; } = default!;
+        public DateTime DateAdded { get; set; }
+        public long FileSize { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ ComicSort.Data/Entities/ComicBookEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicSort.Data.Entities
+{
+    public class ComicBookEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string FilePath { get; set; } = default!;
+
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
+        public long FileSize { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
