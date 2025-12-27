02fdaa6fde2f53e7d0c812d43cdddc1414c3a325
Zath <51383195+Zatharus@users.noreply.github.com>
2025-12-27 13:34:58 -0500

    Wired Browse Button in ProfileDialog
    Wired Browse Button in ProfileDialog window

---
+++ ComicSort.UI/Models/StorageType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicSort.UI.Models
+{
+    public enum StorageType
+    {
+        SQLite,
+        XML
+        
+    }
+}
--- ComicSort.UI/ViewModels/ProfileDialogViewModel.cs
+++ ComicSort.UI/ViewModels/ProfileDialogViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Avalonia.Controls;
+using ComicSort.UI.Models;
 using ComicSort.UI.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
 
 
 namespace ComicSort.UI.ViewModels
@@ -8,7 +12,19 @@ namespace ComicSort.UI.ViewModels
     public partial class ProfileDialogViewModel : ViewModelBase
     {
         private readonly IDialogServices _dialogServices;
-        
+
+        public Array StorageTypes { get; } =
+        Enum.GetValues(typeof(StorageType));
+
+        [ObservableProperty]
+        private StorageType selectedStorageType;
+
+        [ObservableProperty]
+        private string profileName = string.Empty;
+
+        [ObservableProperty]
+        private string selectedPath = string.Empty;
+
         public ProfileDialogViewModel(IDialogServices dialogServices)
         {
             _dialogServices = dialogServices;
@@ -18,6 +34,15 @@ namespace ComicSort.UI.ViewModels
         private void OkButton(Window window)
         {
            window.Close(true);
+        }
+
+        [RelayCommand]
+        private async Task BrowseButton()
+        {    
+            var folderPath = await _dialogServices.ShowOpenFolderDialogAsync("Select profile storage location");
+            if (folderPath is null)
+                return;
+            SelectedPath = folderPath;
         }
 
     }
